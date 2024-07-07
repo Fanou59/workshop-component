b0d5de0c96dd3eb596e547f8e98ec5cd6536d313
Ste[PH]ane <steph.guery@gmail.com>
2024-07-07 18:24:35 +0200

    init

--- app/page.js
+++ app/page.js
@@ -2,6 +2,11 @@ export default function Home() {
   return (
     <main>
       <header>Todo</header>
+      {/* Création d'un composant Header pour afficher titre de l'app et le logo React 
+      -> création d'un Button (et utiliser CVA) pour les 2 boutons du header
+      -> les icones provenant de lucid-react sont ShoppingBasket et User
+      
+      */}
     </main>
   );
 }
