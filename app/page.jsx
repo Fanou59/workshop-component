d8445ccc0b3bdc09bbd3a48ae9c81b8afa704995
Ste[PH]ane <steph.guery@gmail.com>
2024-07-15 19:29:36 +0200

    Ok !

--- app/page.jsx
+++ app/page.jsx
@@ -4,29 +4,29 @@ import { Navbar } from "@/src/Navbar";
 import { REACT_CARDS } from "@/src/ReactCards";
 
 export default function Home({searchParams}) {
-  const currentFilter = searchParams.currentFilter
-  const filters = [... new Set(REACT_CARDS.map((c)=>c.category))]
-  // const category = props.searchParams.category
+  const currentFilter = searchParams.filter
+  const filters = [... new Set(REACT_CARDS.map((card)=>card.category))]
+
   return (
     <main className="m-auto flex h-full max-w-4xl flex-col px-4">
       <Header />
       <div className="mb-4 mt-8 flex flex-1 gap-4 overflow-auto max-lg:flex-col">
-        <Navbar filters = {filters}/>
+        <Navbar currentFilter={currentFilter} filters= {filters}/>
         <div className="size-full overflow-auto">
           <div className="grid h-fit w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-            {/* On doit afficher autant de card que le fichier ReactCards contien d'entrée */}
+
             {REACT_CARDS.filter ((card)=> {
               if (!currentFilter) return true;
               return card.category === currentFilter;
-            }).map((card, index) => (
-              <Card
-                key={card.index}
+            }).map((card) => (
+              <Card hideCategory={currentFilter}
+                key={card.name}
                 name={card.name}
                 category={card.category}
                 url={card.url}
               />
             ))}
-            {/* <Card /> */}
+
           </div>
         </div>
       </div>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,23 +3,23 @@ import { REACT_CARDS } from "./ReactCards";
 import Link from "next/link";
 import clsx from "clsx";
 
-// const uniqueCategories = [
-//   "All",
-//   ...new Set(REACT_CARDS.map((card) => card.category)),
-// ];
-
-export const Navbar = ({filters}) => {
+export const Navbar = ({filters, currentFilter}) => {
   return (
     <nav className="flex w-full flex-wrap gap-4 lg:max-w-[200px] lg:flex-col">
+      <CategoryLinks isActivefilter={!currentFilter} filter="" key="filter">
+        All
+        </CategoryLinks>
       {filters.map((filter) => (
-        <CategoryLinks filter = {filter}/>
+        <CategoryLinks isActive={filter === currentFilter} filter = {filter} key={filter} >
+        {filter}
+        </CategoryLinks>
       ))}
       
     </nav>
   );
 };
 
-const CategoryLinks = ({filter}) => {
+const CategoryLinks = ({filter, children, isActive}) => {
   return (
     <>
         <Link
@@ -27,23 +27,14 @@ const CategoryLinks = ({filter}) => {
             "rounded-md px-2 py-1 capitalize transition-colors hover:bg-gray-200",
             {
               "font-bold":
-                activeFilter === (filter === "All" ? "" : filter),
+                isActive,
             }
           )}
           href={`/?filter=${filter}`}
           key={filter}
         >
-          {filter}
+          {children}
         </Link>
     </>
   );
 };
-/*
-Pour le menu, tu vas utiliser les Link en NextJS en utilisant des SearchParams.
-
-En gros, l'URL va ressembler à ça : /?filter=hooks en remplaçant hooks par la catégorie.
-
-Il faudra aussi ajouter une catégorie manuellement all qui va afficher toutes les cartes. (pour ça, il faut supprimer le searchParams de l'URL)
-
-De ce fait, quand on clique sur un lien, on va changer l'URL et donc le contenu de la page. (qu'on fera dans la partie 3)
-*/
